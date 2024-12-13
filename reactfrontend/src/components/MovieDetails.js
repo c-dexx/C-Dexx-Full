084a07eb113ac6820bb1432e52f13d3db4aa58e7
maozora <farrellapple28@gmail.com>
2024-12-13 08:51:56 +0700

    fix movie not read

--- reactfrontend/src/components/MovieDetails.js
+++ reactfrontend/src/components/MovieDetails.js
@@ -1,7 +1,7 @@
 import { Box, Image, Text, VStack, HStack, Button, Textarea, Input, Flex ,Badge} from '@chakra-ui/react';
 import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import moviesData from './movielist.json';
 import AuthContext from './AuthContext';
 
 const MovieDetails = () => {
@@ -32,12 +32,8 @@ const MovieDetails = () => {
   };
 
   useEffect(() => {
-    const fetchMovieDetails = async () => {
-      const response = await axios.get(`http://www.omdbapi.com/?apikey=80e7807a&i=${id}`);
-      setMovie(response.data);
-    };
-
-    fetchMovieDetails();
+    const movie = moviesData.movies.find((movie) => movie.id === parseInt(id));
+    setMovie(movie);
   }, [id]);
 
   const handleAddReview = () => {
@@ -99,25 +95,8 @@ const MovieDetails = () => {
           </Box>
           <Text fontSize="48px" fontWeight="800" textColor="white" mb={"6"}>{movie.Title} <span style={{opacity: 0.5, marginLeft:"16px"}}>{movie.Year}</span></Text>
           <Box display="flex" alignItems="center" mb="6">
-              <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                width="32px" 
-                height="32px" 
-                viewBox="0 0 24 24" 
-                fill="none"
-                style={{ marginRight: '4px' }}
-              >
-                <g clipPath="url(#clip0_54_12)">
-                  <path d="M11.5 12.5L12 11V10.5L13 11L11.2844 11.1386L12 10L13.5 11.5L11.5 11L11.9307 11.1386L11.5 12.5ZM5.825 20L8.15 12.4L2 8H9.6L12 0L14.4 8H22L15.85 12.4L18.175 20L12 15.3L5.825 20Z" fill="#37FF00"/>
-                </g>
-                <defs>
-                  <clipPath id="clip0_54_12">
-                    <rect width="24" height="24" fill="white"/>
-                  </clipPath>
-                </defs>
-              </svg>
               <Text fontWeight="800" fontSize="32px" textColor={"white"}>
-                {movie.imdbRating || "N/A"} / 10
+                {movie.rating || "N/A"} / 10
               </Text>
             </Box>
           <Text mb="1" textColor={"white"} fontSize={"24px"} fontWeight={"500"}><strong>Overview :</strong></Text>
